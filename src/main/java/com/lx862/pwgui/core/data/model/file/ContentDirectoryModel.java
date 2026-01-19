721af769cbbb62653c61e543a750cccf8e9414e7
AmberFrost <kenny.mh.hui@outlook.com>
2026-01-19 16:59:20 +0800

    Add (partial) icon for content directory

--- src/main/java/com/lx862/pwgui/core/data/model/file/ContentDirectoryModel.java
+++ src/main/java/com/lx862/pwgui/core/data/model/file/ContentDirectoryModel.java
@@ -1,16 +1,20 @@
 package com.lx862.pwgui.core.data.model.file;
 
+import com.lx862.pwgui.util.GUIHelper;
+import com.lx862.pwgui.util.Util;
+
+import javax.swing.*;
 import java.io.File;
 import java.util.HashMap;
 
 public class ContentDirectoryModel extends DirectoryModel {
-    private static final HashMap<String, String> directoryToNameMap = new HashMap<>();
+    private static final HashMap<String, NameIcon> directoryToNameMap = new HashMap<>();
 
     static {
-        directoryToNameMap.put("mods", "Mods");
-        directoryToNameMap.put("resourcepacks", "Resource Packs");
-        directoryToNameMap.put("shaderpacks", "Shader Packs");
-        directoryToNameMap.put("plugins", "Plugins");
+        directoryToNameMap.put("mods", new NameIcon("Mods", new ImageIcon(GUIHelper.convertImage(Util.getAssets("/assets/mime/mods.png"), 16))));
+        directoryToNameMap.put("resourcepacks", new NameIcon("Resource Packs", null));
+        directoryToNameMap.put("shaderpacks", new NameIcon("Shader Packs", new ImageIcon(GUIHelper.convertImage(Util.getAssets("/assets/mime/shaderpacks.png"), 16))));
+        directoryToNameMap.put("plugins", new NameIcon("Plugins", null));
     }
 
     public ContentDirectoryModel(File file) {
@@ -20,11 +24,20 @@ public class ContentDirectoryModel extends DirectoryModel {
     @Override
     public String getDisplayName() {
         String fileName = path.toFile().getName();
-        return directoryToNameMap.getOrDefault(fileName, fileName);
+        return directoryToNameMap.getOrDefault(fileName, new NameIcon(fileName, null)).name();
     }
 
     @Override
     public boolean isUserFriendlyName() {
         return true;
     }
+
+    @Override
+    public Icon getIcon() {
+        String fileName = path.toFile().getName();
+        Icon icon = directoryToNameMap.getOrDefault(fileName, new NameIcon(fileName, null)).icon();
+        return icon == null ? super.getIcon() : icon;
+    }
+
+    record NameIcon(String name, Icon icon) {}
 }
